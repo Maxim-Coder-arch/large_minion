0e54120b9dd33cb7140dcb1eb391879c245e9f51
Maxim-Coder-arch <maximnovikov2206@gmail.com>
2026-03-03 15:35:16 +0300

    add coments

--- app/graduates/[slug]/page.tsx
+++ app/graduates/[slug]/page.tsx
@@ -1,7 +1,18 @@
-import { graduatesData } from "@/app/data/graduates/graduates.data";
+// app/graduates/page.js
+import { getDB } from '@/lib/mongodb';
 import TemplateBlank from "@/app/genercis/templateBlank";
 
-export default function Page() {
+export default async function Page() {
+  // Подключаемся к БД
+  const db = await getDB();
+  
+  // Получаем всех выпускников из БД
+  const graduatesData = await db
+    .collection('graduates')
+    .find({})
+    .sort({ id: 1 }) // сортируем по id
+    .toArray();
+
   return (
     <div>
       <TemplateBlank petsData={graduatesData} />
